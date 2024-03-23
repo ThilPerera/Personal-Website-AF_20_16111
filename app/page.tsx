40a1be326143ee8dcc76ea40987a11313b7f2528
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-23 17:37:53 +0530

    Add the new sections to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import SectionDivider from "@/components/Section-divider";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center px-4">
       <Header></Header>
       <Intro></Intro>
       <SectionDivider></SectionDivider>
+      <About></About>
     </main>
   );
 }
