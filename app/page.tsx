8ae04867829f27acc2eea8e52b7d71a40732f571
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-23 15:36:14 +0530

    Add section divider to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
+import SectionDivider from "@/components/Section-divider";
 
 export default function Home() {
   return (
     <main className="">
       <Header></Header>
       <Intro></Intro>
+      <SectionDivider></SectionDivider>
     </main>
   );
 }
