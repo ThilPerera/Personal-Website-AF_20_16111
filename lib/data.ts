d45883e3558b050fdfe844ab45a28bb2b9e1c848
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-24 15:54:35 +0530

    Adding the project data to the data file.

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,5 @@
+import BookU from "@/public/images/BookU.png";
+
 export const links = [
     {
       name: "Home",
@@ -23,4 +25,22 @@ export const links = [
       name: "Contact",
       hash: "#contact",
     },
+  ] as const;
+
+  export const projectsData = [
+    {
+      title: "BookU",
+      description:
+        "This project is brought out as a personal project in which I took the responsibility of both the dreamer and the architect as the sole UX/UI designer.",
+      tags: ["UX", "UI", "Figma", "Research",],
+      imageUrl: BookU,
+    },
+    {
+      title: "rmtDev",
+      description:
+        "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+      tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      imageUrl: BookU,
+    },
+  
   ] as const;
