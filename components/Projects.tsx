3374ff6fc7387428a46abcf856dffd7f59c6ff0b
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-24 17:38:10 +0530

    Styling the project cards

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -25,17 +25,21 @@ type Projectprops=(typeof projectsData)[0];
 function Project({title,description,tags,imageUrl}:
     Projectprops){
         return(
-            <section>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <ul>
+            <section className='bg-purple-400 bg-opacity-10 max-w-[42rem]border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last-mb-0'>
+              <div className='py-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+              <h3 className='text-2xl font-semibold'>{title}</h3>
+                <p className='mt-2 leading-relaxed text-gray-400'>{description}</p>
+                <ul className='flex flex-wrap mt-4 ga-2 sm:m-auto'>
                     {
                         tags.map((tag,index)=>(
-                            <li key={index}>{tag}</li>
+                            <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full m-1' key={index}>{tag}</li>
                         ))
                     }
                 </ul>
-               <Image src={imageUrl} alt={title}></Image>
+                
+              </div>
+                
+               <Image className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl' src={imageUrl} alt={title} quality={95}></Image>
             </section>
             );
         
