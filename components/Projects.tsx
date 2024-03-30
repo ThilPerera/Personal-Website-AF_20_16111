c879ebf90c47e10e8a6b3b76a5739b49697dee62
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-30 12:33:20 +0530

    change the hover effect of the project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,48 +1,53 @@
-import React from 'react'
-import SectionHeading from './Section-heading'
-import SectionDivider from './Section-divider'
-import {projectsData} from '../lib/data'
-import Image from 'next/image'
+import React from 'react';
+import SectionHeading from './Section-heading';
+import SectionDivider from './Section-divider';
+import { projectsData } from '../lib/data';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
   return (
-    <section className='justify-item-center'>
-        <SectionHeading>My Projects</SectionHeading>
-        <div>
+    <section className='justify-item-center scroll-mt-28'
+    id='projects' >
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
           </React.Fragment>
         ))}
-
-        </div>
+      </div>
     </section>
-  )
+  );
+};
+
+type ProjectProps = typeof projectsData[number];
+
+function Project({ title, link, description, tags, imageUrl }: ProjectProps ) {
+  return (
+    <section className='bg-purple-400 bg-opacity-10 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last-mb-0 group' 
+    id='projects' >
+      <div className='py-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+        <a className='text-2xl font-semibold ' href={link}>{title}</a>
+        <p className='mt-2 leading-relaxed text-gray-400'>{description}</p>
+        <ul className='flex flex-wrap mt-4 ga-2 sm:m-auto'>
+          {tags.map((tag, index) => (
+            <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full m-1' key={index}>
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+        <Image src={imageUrl} alt={title} quality={95} className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+      transition 
+      group-hover:scale-[1.04]
+      group-hover:-translate-x-3
+      group-hover:translate-y-3
+      group-hover:-rotate-2
+
+     ' />
+    </section>
+  );
 }
 
-type Projectprops=(typeof projectsData)[0];
-
-function Project({title,description,tags,imageUrl}:
-    Projectprops){
-        return(
-            <section className='bg-purple-400 bg-opacity-10 max-w-[42rem]border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last-mb-0'>
-              <div className='py-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
-              <h3 className='text-2xl font-semibold'>{title}</h3>
-                <p className='mt-2 leading-relaxed text-gray-400'>{description}</p>
-                <ul className='flex flex-wrap mt-4 ga-2 sm:m-auto'>
-                    {
-                        tags.map((tag,index)=>(
-                            <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full m-1' key={index}>{tag}</li>
-                        ))
-                    }
-                </ul>
-                
-              </div>
-                
-               <Image className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl' src={imageUrl} alt={title} quality={95}></Image>
-            </section>
-            );
-        
-    }
-
-export default Projects
+export default Projects;
