f94822aecab03879cef21bc43eda0f9271a55686
ThilPerera <ar101942@fhss.sjp.ac.lk>
2024-03-31 22:42:55 +0530

    Add achievement and contact to the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import About from "@/components/About";
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
+import Achievements  from "@/components/Achievements";
 import SectionDivider from "@/components/Section-divider";
 import { Skill } from "@/components/Skill";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -17,6 +19,11 @@ export default function Home() {
       <SectionDivider>
       </SectionDivider>
       <Skill></Skill>
+      <SectionDivider></SectionDivider>
+      <Achievements></Achievements>
+      <SectionDivider>
+      </SectionDivider>
+      <Contact></Contact>
     </main>
   );
 }
